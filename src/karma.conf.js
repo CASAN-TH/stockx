9de06e164dce9dcda5829b72801bad35c9fb1e74
jigkoh3 <jigkoh3@hotmail.com>
2018-05-12 14:22:21 +0700

    [edited] config.yml for fix issue of node version for ng test

--- src/karma.conf.js
+++ src/karma.conf.js
@@ -16,16 +16,16 @@ module.exports = function (config) {
     client: {
       clearContext: false // leave Jasmine Spec Runner output visible in browser
     },
-    // coverageIstanbulReporter: {
-    //   dir: require('path').join(__dirname, '../coverage'),
-    //   reports: ['html', 'lcovonly'],
-    //   fixWebpackSourcePaths: true
-    // },
+    coverageIstanbulReporter: {
+      dir: require('path').join(__dirname, '../coverage'),
+      reports: ['html', 'lcovonly'],
+      fixWebpackSourcePaths: true
+    },
     //reporters: ['progress', 'kjhtml'],
     reporters: ['mocha'],
     port: 9876,
     colors: true,
-    logLevel: config.LOG_INFO,
+    //logLevel: config.LOG_INFO,
     autoWatch: true,
     browsers: ['Chrome'],
     singleRun: true
